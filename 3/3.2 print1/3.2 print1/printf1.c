53a005e7ce00c6bbbf04c6ad2bfcdd2b86c72d30
theloki2000 <1208850820@qq.com>
2024-08-16 22:50:51 +0800

    Bai

---
+++ 3/3.2 print1/3.2 print1/printf1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int ten = 10;
+	int two = 2;
+	printf("%d,%d,%d\n", ten, 2, ten - two);
+	printf("%d,%d,%d\n", ten);
+	return 0;
+}
+/*
+10,2,8
+10, -641, 0
+*/
---
+++ 3/3.3 bases/3.3 bases/bases.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int x = 100;
+	printf("%d,%o,%x\n", x, x, x);
+	printf("%d,%#o,%#x\n", x, x, x);
+	return 0;
+}
+/*
+100, 144, 64
+100, 0144, 0x64
+*/
